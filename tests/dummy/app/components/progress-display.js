51b0432d3d092ff7490519d97c182bc60be421f0
Chris Bonser <bonser.chris@gmail.com>
2019-03-20 11:44:18 -0700

    instrument render time

--- tests/dummy/app/components/progress-display.js
+++ tests/dummy/app/components/progress-display.js
@@ -5,6 +5,7 @@ export default Component.extend({
   layout,
   actions: {
     start(currentPage, endPage) {
+      this.set("startTimeStamp", new Date());
       this.set("isRunning", true);
       this.set("currentPage", currentPage);
       this.set("endPage", endPage);
@@ -14,7 +15,9 @@ export default Component.extend({
       this.set("endPage", endPage);
     },
     complete() {
+      this.set("renderTime", (new Date() - this.startTimeStamp) / 1000);
       this.set("isRunning", false);
+      this.set("isComplete", true);
     }
   }
 });
