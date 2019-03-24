bae4d7642820c67c97bbb659ee656ba6b715a135
Chris Bonser <bonser.chris@gmail.com>
2019-03-24 17:13:19 -0500

    remove npm lock file; use ember try on ci

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,56 +1,60 @@
-'use strict';
+"use strict";
 
-const getChannelURL = require('ember-source-channel-url');
+const getChannelURL = require("ember-source-channel-url");
 
 module.exports = function() {
   return Promise.all([
-    getChannelURL('release'),
-    getChannelURL('beta'),
-    getChannelURL('canary')
-  ]).then((urls) => {
+    getChannelURL("release"),
+    getChannelURL("beta"),
+    getChannelURL("canary")
+  ]).then(urls => {
     return {
+      command: "ember test --silent --reporter xunit > ~/ember/junit.xml",
+      useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-2.18',
+          name: "ember-lts-2.18",
           env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
+            EMBER_OPTIONAL_FEATURES: JSON.stringify({
+              "jquery-integration": true
+            })
           },
           npm: {
             devDependencies: {
-              '@ember/jquery': '^0.5.1',
-              'ember-source': '~2.18.0'
+              "@ember/jquery": "^0.5.1",
+              "ember-source": "~2.18.0"
             }
           }
         },
         {
-          name: 'ember-lts-3.4',
+          name: "ember-lts-3.4",
           npm: {
             devDependencies: {
-              'ember-source': '~3.4.0'
+              "ember-source": "~3.4.0"
             }
           }
         },
         {
-          name: 'ember-release',
+          name: "ember-release",
           npm: {
             devDependencies: {
-              'ember-source': urls[0]
+              "ember-source": urls[0]
             }
           }
         },
         {
-          name: 'ember-beta',
+          name: "ember-beta",
           npm: {
             devDependencies: {
-              'ember-source': urls[1]
+              "ember-source": urls[1]
             }
           }
         },
         {
-          name: 'ember-canary',
+          name: "ember-canary",
           npm: {
             devDependencies: {
-              'ember-source': urls[2]
+              "ember-source": urls[2]
             }
           }
         },
@@ -59,21 +63,21 @@ module.exports = function() {
         // `ember try:each` manually or from a customized CI config will run it
         // along with all the other scenarios.
         {
-          name: 'ember-default',
+          name: "ember-default",
           npm: {
             devDependencies: {}
           }
         },
         {
-          name: 'ember-default-with-jquery',
+          name: "ember-default-with-jquery",
           env: {
             EMBER_OPTIONAL_FEATURES: JSON.stringify({
-              'jquery-integration': true
+              "jquery-integration": true
             })
           },
           npm: {
             devDependencies: {
-              '@ember/jquery': '^0.5.1'
+              "@ember/jquery": "^0.5.1"
             }
           }
         }
