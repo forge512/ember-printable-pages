8bc18384d213016640a125a18c581242ec0358ed
Chris Bonser <chris.bonser@gmail.com>
2019-09-26 11:03:12 -0500

    fix issue where a single item overflowing to the last page of a chapter would prevent the render complete callback from getting called

--- tests/acceptance/large-doc-homogenous-items-test.js
+++ tests/acceptance/large-doc-homogenous-items-test.js
@@ -1,14 +1,14 @@
-import { module, test, skip } from "qunit";
+import { module, test } from "qunit";
 import { fillIn, find, visit } from "@ember/test-helpers";
 import { setupApplicationTest } from "ember-qunit";
+import { timeout } from "ember-concurrency";
 
 module("Acceptance | large doc homogenous items", function(hooks) {
   setupApplicationTest(hooks);
 
   // TODO add nested modules for various page sizes
 
-  // This fails only on CI... sigh
-  skip("3-column mode, single page, with header and footer", async function(assert) {
+  test("3-column mode, single page, with header and footer", async function(assert) {
     await visit("/demos/large?columnCount=3&sectionCount=33");
     assert.dom("[data-test-page]").exists({ count: 1 });
     assert.dom("[data-test-page='1'] [data-test-page-header]").exists();
