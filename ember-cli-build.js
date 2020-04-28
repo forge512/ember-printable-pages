4a247693a077fc4583cf028598b22bcd5365e654
betocantu93 <betocantu93@gmail.com>
2020-04-28 15:32:14 -0500

    Revert "try removing ember-cli-build"
    This reverts commit 6f9307b8bc094c36374cb7e674473851e8a46269.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,9 +1,45 @@
 "use strict";
 
 const EmberAddon = require("ember-cli/lib/broccoli/ember-addon");
+const isProduction = EmberAddon.env() === "production";
+
+const purgeCSS = {
+  module: require("@fullhuman/postcss-purgecss"),
+  options: {
+    content: [
+      // add extra paths here for components/controllers which include tailwind classes
+      "./tests/dummy/app/index.html",
+      "./tests/dummy/app/templates/**/*.hbs"
+    ],
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  }
+};
 
 module.exports = function(defaults) {
-  
+  let app = new EmberAddon(defaults, {
+    "ember-cli-uglify": {
+      uglify: {
+        compress: {
+          collapse_vars: false
+        }
+      }
+    },
+    snippetPaths: ["tests/dummy/app/templates/demos"],
+    postcssOptions: {
+      compile: {
+        plugins: [
+          {
+            module: require("postcss-import"),
+            options: {
+              path: ["node_modules"]
+            }
+          },
+          require("tailwindcss")("./tests/dummy/app/tailwind/config.js"),
+          ...(isProduction ? [purgeCSS] : [])
+        ]
+      }
+    }
+  });
 
   /*
     This build file specifies the options for the dummy test app of this
