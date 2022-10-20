9c23afec1830ba0118fae8178cfb50a298bdb3d9
Chris Bonser <bonser.chris@gmail.com>
2022-10-20 15:19:15 -0400

    remove the unused modifier set-page-body-height

--- addon/modifiers/set-page-body-height.js
+++
@@ -1,5 +0,0 @@
-import { modifier } from 'ember-modifier';
-
-export default modifier(function setPageBodyHeight(element/*, positional, named*/) {
-
-});
--- app/modifiers/set-page-body-height.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-printable-pages/modifiers/set-page-body-height';
